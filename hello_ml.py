c39a909f357511184309175f8167d7cd25c059ab
sachink786 <pgdai2835@gmail.com>
2025-06-25 09:05:08 +0530

    hello_ml.py

--- hello_ml.py
+++ hello_ml.py
@@ -1,10 +1,9 @@
 import platform
 import os
-import psutil
 
 
 print("Hello, ML Worrld from Jenkins!")
 print(f"Python Version:{platform.python_version()}")
 print(f"Operating System :{platform.system()}{platform.release()}")
 print(f"CPU Cores:{os.cpu_count()}")
-print(f"Total RAM :{round(psutil.virtual_memory().total/(1024**3),2)} GB")
+
